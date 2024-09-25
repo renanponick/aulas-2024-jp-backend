205fd6f80d6b377d898bb13165e6b1b085317375
Renan <warreir@gmail.com>
2024-09-25 19:38:22 -0300

    aula-service

--- index.js
+++ index.js
@@ -1,57 +1,42 @@
 const express = require('express');
+const {
+    somar,
+    calculaSalario,
+    verificaTriangulos2
+} = require('./service/exercicios');
+
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.get("/exercicio1/", (req, res) => {
     // http://localhost:3000/exercicio1/?num1=0&num2=3
-    const num1 = req.query.num1
-    const num2 = req.query.num2
-
-    const resultado = Number(num1) + Number(num2);
+    const resultado = somar(req.query.num1, req.query.num2);
 
     res.json({ resultadoGet: resultado })
 })
 
 app.get("/exercicio2/", (req, res) => {
     // http://localhost:3000/exercicio2/?valorHora=0&horasTrabalhadas=3
-    const valorHora = Number(req.query.valorHora)
-    const horasTrabalhadas = Number(req.query.horasTrabalhadas)
-
-    const salario = valorHora * horasTrabalhadas;
-    // res.status - status de retorno
-    // res.json() - envio do retorno
-    // res.send() - envio do retorno
+    const salario = calculaSalario(
+        Number(req.query.valorHora),
+        Number(req.query.horasTrabalhadas)
+    );
     res.json({ resultado: salario })
 })
 
 app.post("/exercicio1/", (req, res) => {
     // http://localhost:3000/exercicio1
-    const num1 = req.body.num1
-    const num2 = req.body.num2
-
-    const resultado = Number(num1) + Number(num2);
+    const resultado = somar(req.body.num1, req.body.num2);
 
     // informar um status diferente de 200 (pesquisar sobre 😊)
     res.status(201).json({ resultadoPost: resultado })
 })
 
-
 app.post("/exercicio15/", (req, res) => {
     // http://localhost:3000/exercicio1
     const { a, b, c } = req.body
-
-    if ((a + b < c) || (a + c < b) || (b + c < a) ){
-        resultado = 'Nao é um triangulo'
-    } else if ((a == b) || (a == c)) {
-        resultado = 'Equilatero'
-    } else if ((a==b) || (a==c) || (b==c)) {
-        resultado = 'Isósceles'
-    } else {
-        resultado = 'Escaleno'
-    }
-
-    // informar um status diferente de 200 (pesquisar sobre 😊)
+    const resultado = verificaTriangulos2(a,b,c)
     res.status(201).json({ resultado })
 })
 
