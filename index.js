ef474c62f635c8f54562dffd963729e55cf451e5
Renan <warreir@gmail.com>
2024-11-13 18:54:32 -0300

    done paths

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-// Não é mais tudo !!!!
 const express = require('express')
-const routers = require('./src/routes/pessoa')
+const routersPerson = require('./src/routes/pessoa')
 const database = require('./src/config/database')
 
 const app = express()
 app.use(express.json())
-app.use(routers)
+app.use("/person", routersPerson)
 const PORT = 3000
 
 database.db
